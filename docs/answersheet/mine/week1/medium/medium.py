d41523390128c67582afa7c084c32de14bc114cf
Mahendra-Palagiri <Mahendra.Palagiri@outlook.com>
2025-09-22 13:55:37 -0400

    Week1 easy and medium questions and coding challange completed.

---
+++ docs/answersheet/mine/week1/medium/medium.py
@@ -0,0 +1,72 @@
+'''1.	Write a function to calculate factorial of a number.
+2.	Write a function that returns the sum of all elements in a list.
+3.	Write a program that prints the multiplication table of a number.
+4.	Write a function that checks if a word is a palindrome.
+5.	Implement a simple calculator with functions for add, subtract, multiply, divide.'''
+
+#1.	Write a function to calculate factorial of a number
+def calc_factorial(num):
+    fact =1
+    for i in range(1,num+1):
+        fact  = fact * i
+
+    return fact
+
+print(calc_factorial(5))
+
+#2. Write a function that returns the sum of all elements in a list
+def get_sum(nums):
+    return sum(nums)
+
+print("Sum of numbers [1,4,7] --> ",get_sum([1,4,7]))
+
+#3. Write a program that prints the multiplication table of a number.
+def print_multpltable(num):
+    for i in range(1,11):
+        print(f"{num} X {i} = {num*i}")
+
+print_multpltable(7)
+
+#4. Write a program that prints the multiplication table of a number.
+import math as m
+
+def is_palindrome(val):
+    rangetocheck  = m.floor(len(val)/2)
+    palcheck = True
+    for i in range(rangetocheck):
+        if val[i] != val[len(val)-(i+1)]:
+            palcheck = False
+            break
+
+    return palcheck
+
+word = 'tattarrattat'
+print(is_palindrome(word))
+
+
+#5.	Implement a simple calculator with functions for add, subtract, multiply, divide.'''
+
+class mymath:
+
+    def __init__(self,a,b):
+        self.a = a
+        self.b = b
+    
+    def add(self):
+        print (f"addtion of {self.a} and {self.b} = {self.a + self.b}")
+
+    def substract(self):
+        print (f"substraction of {self.a} and {self.b} = {self.a - self.b}")
+
+    def multiply(self):
+        print (f"multiplication of {self.a} and {self.b} = {self.a * self.b}")
+
+    def divide(self):
+        print (f"division of {self.a} and {self.b} = {self.a / self.b}")
+
+
+mymathclass = mymath(8,2)
+mymathclass.add()
+mymathclass.substract()
+mymathclass.multiply()
+mymathclass.divide()
---
+++ docs/answersheet/mine/week1/medium/medium_questions.py
@@ -0,0 +1,34 @@
+#--------------------------------------------------------------------
+# Following program is related to questions
+#--------------------------------------------------------------------
+# Q.1
+def isprime(num):
+    divisble = False
+    for i in range(2,num):
+        if num % i ==0 :
+            divisble = True
+            break
+
+    return 'Non-Prime' if divisble else 'Prime'
+
+print(isprime(int(input("Enter a number\n"))))
+
+# Q.11
+def reverse_list(nums):
+    revlist  = []
+    iterable = len(nums)
+    while iterable > 0:
+        revlist.append(nums[iterable-1])
+        iterable = iterable-1
+
+    return revlist
+
+nums  = [3,4,5,6,7]
+print(reverse_list(nums))
+
+#Q. 12
+
+mlist  = ['ra','ga','sa','ri']
+mlist.remove('ra')
+mlist.remove(mlist[1])
+print(mlist)
